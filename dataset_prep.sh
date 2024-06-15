da74da81c065c0bba4aab87ebcc5deaed161c23d
Hamid Irannejad <134910414+hamid-irannejad@users.noreply.github.com>
2024-06-15 11:49:50 +0330

    Delete dataset_prep.sh

--- dataset_prep.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-cat ido_raw.tsv | cut -f 1,8,11 | sort -k1 | uniq -w 20 | head -n 2842 > ido_3col
-cat tdo_raw.tsv | cut -f 1,8,11 | sort -k1 | uniq -w 20 | sed -e 's/"//g' | head -n 895 > tdo_3col
-
-echo "CHEMBLID smiles ido_ic50 tdo_ic50" > final_total
-
-join ido_3col tdo_3col | cut -d " " -f 1,2,3,5 >> final_total
-
